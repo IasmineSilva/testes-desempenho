cbd4719986f17aa1ee15806b571bf6880e25f9ff
Iasmine <Iasmine948@gmail.com>
2026-04-17 23:23:00 -0300

    .

--- K6testes/teste1.js
+++ K6testes/teste1.js
@@ -3,9 +3,9 @@ import { check, sleep } from 'k6';
 
 export const options = {
     stages: [
-        { duration: '30s', target: 30 }, // Ramp-up para 30 usuários em 30 segundos
-        { duration: '1m', target: 10 },  // Manter 30 usuários por 1 minuto
-        { duration: '30s', target: 0 },   // Ramp-down para 0 usuários em 30 segundos
+        { duration: '30s', target: 30 }, 
+        { duration: '1m', target: 10 },  
+        { duration: '30s', target: 0 },  
     ],
 };
 
